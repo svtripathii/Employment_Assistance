10c3cd1c0a5ae3a04fd5df60671595e0afd1e6a9
Saurav Tripathi <95672002+svtripathii@users.noreply.github.com>
2024-10-07 20:11:43 +0530

    Update job_fetcher.py

--- job_fetcher.py
+++ job_fetcher.py
@@ -2,8 +2,8 @@ import requests
 import os
 
 # Adzuna API credentials (better to store in environment variables)
-API_ID = os.getenv('ADZUNA_API_ID', '97aab3e2')  # Replace with your Adzuna API ID
-API_KEY = os.getenv('ADZUNA_API_KEY', 'ff4ea4d3eb3cc64ca2b4f7b5e3c9ea37')  # Replace with your Adzuna API Key
+API_ID = os.getenv('ADZUNA_API_ID', '')  # Replace with your Adzuna API ID
+API_KEY = os.getenv('ADZUNA_API_KEY', '')  # Replace with your Adzuna API Key
 
 def get_job_listings(location="Pune", job_title="Data Scientist"):
     url = "https://api.adzuna.com/v1/api/jobs/in/search/1"
